c55d634a3d5b52474d12276b2a2f7653961eac74
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2025-11-03 16:16:05 +0200

    Practice functions basics

---
+++ js/foundations-function-basics.js
@@ -0,0 +1,15 @@
+function add7(num) {
+  return num + 7;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function capitalize(str) {
+  return str.substr(0, 1).toUpperCase() + str.substr(1).toLowerCase();
+}
+
+function lastLetter(str) {
+  return str.substr(-1);
+}
